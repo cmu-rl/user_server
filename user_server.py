095dc24062cc8e6c8643b1e3ecdc8dbc6bda45ce
Brandon Houghton <bhoughton@cmu.edu>
2018-05-26 15:01:52 -0600

    Added basic UDP user server framework

---
+++ user_server.py
@@ -0,0 +1,48 @@
+import json
+import socketserver
+
+class MyUDPHandler(socketserver.BaseRequestHandler):
+    """
+    This class works similar to the TCP handler class, except that
+    self.request consists of a pair of data and client socket, and since
+    there is no connection the client address must be given explicitly
+    when sending data back via sendto().
+    """
+
+    def handle(self):
+        data = self.request[0]
+        socket = self.request[1]
+
+        try:
+            request = json.loads(data)
+        except ValueError as error:
+            #TODO return error
+            return
+        
+        if 'cmd' in request:
+            response = {}
+
+            print(request['cmd'])
+
+            # Form the response
+            if request['cmd'] == 'echo':
+                response['cmd'] = request['cmd']
+            elif request['cmd'] == 'list_users':
+                response['list'] = ['usr1', 'usr2']
+            else:
+                response['error'] = True
+
+            # Send response
+            socket.sendto(json.dumps(response), self.client_address)
+        else:
+            return
+
+        print("{} wrote:".format(self.client_address[0]))
+        print(data)
+
+        
+
+if __name__ == "__main__":
+    HOST, PORT = "0.0.0.0", 9999
+    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
+        server.serve_forever()
